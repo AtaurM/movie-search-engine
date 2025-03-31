88b860fbdb827d98fceec60fcf4d9aa0c57f1d6b
AtaurM <ataurmuhith@gmail.com>
2025-03-30 20:32:56 -0400

    Added search button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,29 @@ function Home() {
         { id: 1, title: "John Wick", release_date: "2020" },
         { id: 2, title: "Terminator", release_date: "1999" },
         { id: 3, title: "The Matrix", release_date: "1998" },
-    ] 
+    ]
 
-    return (<div className="home">
-        <div className="movies-grid">
+    const handleSearch = () => {
+
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
             
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} />
-            ))}
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
         </div>
-    </div>
     )
 }
 
