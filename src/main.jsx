101c4a53175bd63258f96f4ec3c2bfa4b3afba04
AtaurM <ataurmuhith@gmail.com>
2025-04-03 02:58:02 -0400

    Fixed routing for GitHub pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie-search-engine">
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
