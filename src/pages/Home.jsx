3a28b837862e94098b8026041d5ce757bbcb0df1
AtaurM <ataurmuhith@gmail.com>
2025-04-02 22:55:55 -0400

    Added navbar functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "John Wick", release_date: "2020" },
         { id: 2, title: "Terminator", release_date: "1999" },
         { id: 3, title: "The Matrix", release_date: "1998" },
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
     }
 
     return (
@@ -18,10 +23,12 @@ function Home() {
                     type="text"
                     placeholder="Search for movies..."
                     className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">Search</button>
             </form>
-            
+
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
